4e27a857062841abad1e589e48b1136029bb2e99
nkhoi2000 <45933579+nkhoi2000@users.noreply.github.com>
2021-06-28 20:11:26 +0700

    Last UPDATE

--- booking-bus/futa-search-service/src/main/java/com/sad/bus/futa/search/service/FutaTicketService.java
+++ booking-bus/futa-search-service/src/main/java/com/sad/bus/futa/search/service/FutaTicketService.java
@@ -12,9 +12,16 @@ public class FutaTicketService {
 
 	public static List<Object> callFuta() {
 		List<Object> getTickets = Arrays.asList(
-				new FutaTickets(1, "nha xe phương trang", "11/11/2021","293 đinh bộ lĩnh", 100000L),
-				new FutaTickets(2, "nha xe phương trang 1", "12/11/2021","123 nguyễn xí", 200000L),
-				new FutaTickets(3, "nha xe phương trang 2", "13/11/2021","321 chu van an", 200000L));
+				new FutaTickets(1, "nha xe phuong trang", "11/11/2021","BX Miền Tây", 200000L),
+				new FutaTickets(2, "nha xe phuong trang 1", "12/11/2021","BX An Sương", 200000L),
+				new FutaTickets(3, "nha xe phuong trang 2", "11/11/2021","BX An Sương", 200000L),
+				new FutaTickets(4, "nha xe phuong trang 3", "11/11/2021","Hàng Xanh", 150000L),
+				new FutaTickets(5, "nha xe phuong trang 4", "11/11/2021","BX Miền Đông", 200000L),
+				new FutaTickets(6, "nha xe phuong trang 5", "13/11/2021","BX Miền Tây", 200000L),
+				new FutaTickets(7, "nha xe phuong trang 6", "11/11/2021","BX Miền Tây", 300000L),
+				new FutaTickets(8, "nha xe phuong trang 7", "12/11/2021","BX Miền Đông", 150000L),
+				new FutaTickets(9, "nha xe phuong trang 8", "11/11/2021","BX An Sương", 200000L),
+				new FutaTickets(10, "nha xe phuong trang 9", "12/11/2021","Ngã tư Thủ Đức", 200000L));
 		return getTickets;
 	}
 }
--- booking-bus/vexere-search-service/src/main/java/com/sad/bus/vexere/search/service/VexereTicketService.java
+++ booking-bus/vexere-search-service/src/main/java/com/sad/bus/vexere/search/service/VexereTicketService.java
@@ -12,9 +12,15 @@ public class VexereTicketService {
 
 	public static List<Object> callVexere() {
 		List<Object> getTickets = Arrays.asList(
-				new VexereTickets(1, "vexere", "11/11/2021","293 đinh bộ lĩnh", 100000L),
-				new VexereTickets(2, "vexere 1", "12/11/2021","123 nguyễn xí", 200000L),
-				new VexereTickets(3, "vexere 2", "13/11/2021","321 chu van an", 200000L));
+				new VexereTickets(1, "Việt Nhật", "11/11/2021","Bến xe An Sương", 220000L),
+				new VexereTickets(2, "Việt Nhật", "12/11/2021","Ngã Tư Thủ Đức", 250000L),
+				new VexereTickets(3, "Hà Linh", "13/11/2021","Bến xe An Sương", 300000L),
+				new VexereTickets(4, "Hoà Thuận Anh", "13/11/2021","Bến xe An Sương", 150000L),
+				new VexereTickets(5, "Hoà Thuận Anh", "13/11/2021","Ngã Tư Thủ Đức", 200000L),
+				new VexereTickets(7, "Liên Hưng", "13/11/2021","Ngã Tư Thủ Đức", 250000L),
+				new VexereTickets(8, "Hà Linh", "13/11/2021","Ngã Tư Thủ Đức", 150000L),
+				new VexereTickets(9, "Việt Nhật", "13/11/2021","Bến xe An Sương", 200000L),
+				new VexereTickets(10, "Việt Nhật", "13/11/2021","Bến xe An Sương", 180000L));
 		return getTickets;
 	}	
 }
